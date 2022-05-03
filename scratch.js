787e0414d3c0f554bda819bc13487ef76a28ca51
jjlee90 <jlee190@hotmail.com>
2022-05-03 17:13:43 -0400

    added check for bj function

--- scratch.js
+++ scratch.js
@@ -1,26 +1,34 @@
 // Card deck blueprint
 class cardDeck {
     // pass suit of the cards to be made
-    constructor(suit) {
+    constructor(suit, ) {
             this.suit = suit
+            this.blackjack = false
+            this.bet = false
         }
         // method to make 13 cards
     makeDeck(suit) {
         return [
             ["ace", this.suit = suit, 1],
-            ["three", this.suit = suit, 3],
-            ["two", this.suit = suit, 2],
-            ["three", this.suit = suit, 3],
-            ["two", this.suit = suit, 2],
-            ["three", this.suit = suit, 3],
-            ["two", this.suit = suit, 2],
-            ["three", this.suit = suit, 3],
-            ["two", this.suit = suit, 2],
-            ["three", this.suit = suit, 3],
-            ["two", this.suit = suit, 2],
-            ["three", this.suit = suit, 3],
-            ["two", this.suit = suit, 2],
+            ["ace", this.suit = suit, 1],
+            ["ace", this.suit = suit, 1],
+            ["ace", this.suit = suit, 1],
+            ["ace", this.suit = suit, 1],
+            ["ace", this.suit = suit, 1],
+            ["king", this.suit = suit, 10],
+            ["queen", this.suit = suit, 10],
+            ["jack", this.suit = suit, 10],
+            ["ten", this.suit = suit, 10],
+            ["queen", this.suit = suit, 10],
+            ["jack", this.suit = suit, 10],
+            ["ten", this.suit = suit, 10],
         ]
+    }
+    blackjack(card) {
+        if (card[0][2] + card[1][2] === 21) {
+            this.blackjack = true
+            result()
+        }
     }
 }
 
@@ -42,16 +50,39 @@ export { cardDeck }
 //     $("#dealerCards").hide()
 // })
 
+// test array 
+// [
 // ["ace", this.suit = suit, 1],
-// ["king", this.suit = suit, 10],
-// ["queen", this.suit = suit, 10],
-// ["jack", this.suit = suit, 10],
-// ["ten", this.suit = suit, 10],
-// ["nine", this.suit = suit, 9],
-// ["eight", this.suit = suit, 8],
-// ["seven", this.suit = suit, 7],
-// ["six", this.suit = suit, 6],
-// ["five", this.suit = suit, 5],
-// ["four", this.suit = suit, 4],
 // ["three", this.suit = suit, 3],
 // ["two", this.suit = suit, 2],
+// ["three", this.suit = suit, 3],
+// ["two", this.suit = suit, 2],
+// ["three", this.suit = suit, 3],
+// ["two", this.suit = suit, 2],
+// ["three", this.suit = suit, 3],
+// ["two", this.suit = suit, 2],
+// ["three", this.suit = suit, 3],
+// ["two", this.suit = suit, 2],
+// ["three", this.suit = suit, 3],
+// ["two", this.suit = suit, 2],
+// ]
+
+
+// test array 2
+// 
+
+// return [
+//     ["ace", this.suit = suit, 1],
+//     ["king", this.suit = suit, 10],
+//     ["queen", this.suit = suit, 10],
+//     ["jack", this.suit = suit, 10],
+//     ["ten", this.suit = suit, 10],
+//     ["nine", this.suit = suit, 9],
+//     ["eight", this.suit = suit, 8],
+//     ["seven", this.suit = suit, 7],
+//     ["six", this.suit = suit, 6],
+//     ["five", this.suit = suit, 5],
+//     ["four", this.suit = suit, 4],
+//     ["three", this.suit = suit, 3],
+//     ["two", this.suit = suit, 2],
+// ]
