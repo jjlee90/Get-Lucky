fabe57a94efb187b73bdfa8bf61fc64450e516fa
jjlee90 <jlee190@hotmail.com>
2022-05-04 17:44:24 -0400

    fixed bug showing dealer total.

--- scratch.js
+++ scratch.js
@@ -10,19 +10,20 @@ class cardDeck {
     makeDeck(suit) {
         return [
             ["ace", this.suit = suit, 1],
-            ["ace", this.suit = suit, 1],
-            ["ace", this.suit = suit, 1],
-            ["ace", this.suit = suit, 1],
-            ["ace", this.suit = suit, 1],
-            ["ace", this.suit = suit, 1],
             ["king", this.suit = suit, 10],
             ["queen", this.suit = suit, 10],
             ["jack", this.suit = suit, 10],
             ["ten", this.suit = suit, 10],
-            ["queen", this.suit = suit, 10],
-            ["jack", this.suit = suit, 10],
-            ["ten", this.suit = suit, 10],
+            ["nine", this.suit = suit, 9],
+            ["eight", this.suit = suit, 8],
+            ["seven", this.suit = suit, 7],
+            ["six", this.suit = suit, 6],
+            ["five", this.suit = suit, 5],
+            ["four", this.suit = suit, 4],
+            ["three", this.suit = suit, 3],
+            ["two", this.suit = suit, 2],
         ]
+
     }
     blackjack(card) {
         if (card[0][2] + card[1][2] === 21) {
@@ -69,20 +70,33 @@ export { cardDeck }
 
 
 // test array 2
-// 
-
+//         
 // return [
 //     ["ace", this.suit = suit, 1],
 //     ["king", this.suit = suit, 10],
 //     ["queen", this.suit = suit, 10],
 //     ["jack", this.suit = suit, 10],
 //     ["ten", this.suit = suit, 10],
-//     ["nine", this.suit = suit, 9],
-//     ["eight", this.suit = suit, 8],
-//     ["seven", this.suit = suit, 7],
-//     ["six", this.suit = suit, 6],
-//     ["five", this.suit = suit, 5],
-//     ["four", this.suit = suit, 4],
-//     ["three", this.suit = suit, 3],
-//     ["two", this.suit = suit, 2],
+//     ["ace", this.suit = suit, 1],
+//     ["ace", this.suit = suit, 1],
+//     ["ten", this.suit = suit, 10],
+//     ["ace", this.suit = suit, 1],
+//     ["ace", this.suit = suit, 1],
+//     ["ace", this.suit = suit, 1],
+//     ["ace", this.suit = suit, 1],
 // ]
+
+// function dealerDraw(card) {
+//     console.log(dealerArr + "dealerArr inside dealerDraw")
+//     card = getRandomCard()
+
+//     // [0] represents the first card in the array and [2] is the numerical value of the card 
+//     dealerTotal = dealerArr[0][2] + dealerArr[1][2]
+//     console.log(dealerArr + "inside dealerDraw")
+//     console.log(dealerTotal + "inside dealerDraw")
+
+//     dealerCards.innerHTML = dealerTotal
+//     cardImages = cardImg(card[0], card[1])
+//     dealerCardSection.append(cardImages)
+//     console.log(dealerTotal)
+// }
